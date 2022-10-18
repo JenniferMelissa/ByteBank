04bc6f00dd4f471b4cde0dea1083771c5366cf0c
Jennifer Melissa Martins de Sales <jennifer.sales@geap.com.br>
2022-10-18 13:19:56 -0300

    primeiros códigos

---
+++ ByteBank/ContaCorrente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class ContaCorrente
+    {
+        //classe representa alguma coisa que existe no mundo real 
+
+        public int numero_agencia;
+        public string conta;
+        public string titular;
+        public double saldo;
+    }
+}
---
+++ ByteBank/Program.cs
@@ -0,0 +1,13 @@
+﻿
+//new -> operador que me permite criar um novo objeto
+// . -> acessar os dados da classe
+using ByteBank;
+
+ContaCorrente contaDoAndre = new ContaCorrente();
+
+contaDoAndre.titular = "André Silva";
+contaDoAndre.numero_agencia = 15;
+contaDoAndre.conta = "1010-x";
+contaDoAndre.saldo = 100;
+
+Console.WriteLine("Saldo da conta do André = " + contaDoAndre.saldo);
